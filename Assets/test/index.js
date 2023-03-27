b4894c31dfc6b5cbd7ce6e4b17ed46967182f16f
WaqasHaider <engr.waqashaider@gmail.com>
2023-03-27 23:14:23 +0500

    build files added because it contains setup index.html (analytics + messaging)

--- Assets/test/index.js
+++ Assets/test/index.js
@@ -1,6 +1,3 @@
 function ChangeMe(){
 document.getElementById("h1").innerHTML = "Hello World";
 }
-function DeserializeIt(jsonString){
-	console.log(JSON.parse(UTF8ToString("{\"prop14\":{\"prop114\":\"prop224\"}}")));
-}
